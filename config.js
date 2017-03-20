f9881394b339da0a852644c4699ea07cdc68b982
cheesegits <mattmulit@hotmail.com>
2017-03-20 12:33:43 -0400

    updated NODE_ENV to mLab address

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
     global.DATABASE_URL ||
     (process.env.NODE_ENV === 'production' ?
-        'mongodb://localhost/shopping-list' :
+        'mongodb://mongo-shopper:ml4b@ds133670.mlab.com:33670/mongo-shopping-list' :
         'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
